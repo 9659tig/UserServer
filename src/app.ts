9f7e62572e594a76f4af614da00c748826e1785a
Junho Choi <tig9659@ajou.ac.kr>
2024-01-09 00:40:04 +0900

    Chore : CORS 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routers/router';
+import cors from 'cors';
 
 const app = express()
 const port = 3004
 const cookieParser = require('cookie-parser');
 
+app.use(cors())
 app.use(express.urlencoded( {extended : false } ));
 app.use(express.json());
 app.use(cookieParser());
