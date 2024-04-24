e0700aa9ec4551508a2328e8fdc71a9cff10d09c
Junho Choi <tig9659@ajou.ac.kr>
2024-04-25 02:12:06 +0900

    Feat: 알파버전 유저서버

--- src/config/dynamo.ts
+++ src/config/dynamo.ts
@@ -2,11 +2,11 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DYNAMO_ACCESS } from "./secret";
 
 const docClient = new DynamoDBClient({
-    region: 'ap-northeast-2',
-    credentials:{
-        accessKeyId: DYNAMO_ACCESS.KEY,
-        secretAccessKey: DYNAMO_ACCESS.SECRET_KEY,
-    }
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: DYNAMO_ACCESS.KEY,
+    secretAccessKey: DYNAMO_ACCESS.SECRET_KEY,
+  },
 } as any);
 
-export default docClient
+export default docClient;
